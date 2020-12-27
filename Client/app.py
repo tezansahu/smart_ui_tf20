acc837e541d0f05c7280e3a047b4d507668a96db
arya2331 <rishabharya32@gmail.com>
2020-12-27 22:44:01 +0530

    added dummy function for submit button

--- Client/app.py
+++ Client/app.py
@@ -1,20 +1,26 @@
 import streamlit as st
 
-st.title('Smart UI')
-
-st.write('Developed by Tezan, Shreya and Rishabh')
-
-uploaded_file = st.file_uploader("Choose a image file")
-
 
 def imgshow(uploaded_file):
   if uploaded_file is not None:
 	  image= uploaded_file.read()
 	  img = st.image(image, caption='Train image', use_column_width=False)
 
-imgshow(uploaded_file)
 
-st.button("Submit")	  
+def success(value):
+	if(value):
+		st.success('Succesfully Submitted')	  
+
+st.title('Smart UI')
+st.write('Developed by Tezan, Shreya and Rishabh')
+uploaded_file = st.file_uploader("Choose a image file")
+imgshow(uploaded_file)
+value = st.button("Submit")	 # return a boolean value 
+success(value)
+
+
+# if(value):
+# 	st.success('hello')  
 
 
 
